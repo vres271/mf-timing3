f300d56a459bfd0d57d26c53d12b022d545afb71
vres271 <vres271@gmail.com>
2023-06-02 22:42:08 +0400

    riple and some

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { NgxIndexedDBService } from 'ngx-indexed-db';
 import { EntityType } from './shared/models/items.model';
 import { UsersMockData } from './shared/mocks/users.mock';
 import { RacersMockData } from './shared/mocks/racers.mock';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -14,9 +15,10 @@ export class AppComponent{
   constructor(
     private usersService: UsersService,
     private dbService: NgxIndexedDBService,
+    private primengConfig: PrimeNGConfig
   ) {
     this.usersService.generateUsers()
-
+    this.primengConfig.ripple = true;
     this.dbService.bulkGet(EntityType.User, [1]).subscribe((result: any) => {
       if (!result?.[0]) {
         console.log('DB is empty. Adding data');
