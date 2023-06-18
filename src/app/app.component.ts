7cc87c66964919f1b788a4e86f68bb2455485557
vres271 <vres271@gmail.com>
2023-06-18 11:49:29 +0400

    Races mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { EntityType } from './shared/models/items.model';
 import { UsersMockData } from './shared/mocks/users.mock';
 import { RacersMockData } from './shared/mocks/racers.mock';
 import { PrimeNGConfig } from 'primeng/api';
+import { RacesMockData } from './shared/mocks/races.mock';
 
 @Component({
   selector: 'app-root',
@@ -32,6 +33,10 @@ export class AppComponent{
         this.dbService.bulkAdd(EntityType.Racer, RacersMockData)
           .subscribe((result) => {
             console.log(EntityType.Racer, result);
+          });        
+        this.dbService.bulkAdd(EntityType.Race, RacesMockData)
+          .subscribe((result) => {
+            console.log(EntityType.Race, result);
           });        
       }
     });    
---
+++ src/app/shared/mocks/races.mock.ts
@@ -0,0 +1,8 @@
+import { RaceDTO } from '../models/race.model';
+export const RacesMockData: RaceDTO[] = [
+  {id: 1, name: "Медицинка XCO", startDate: 1685250000000, raceType: 1},
+  {name: "LCPF - Левобережный ПампФест", startDate: 1687593600000, raceType: 2, id: 2},
+  {id: 3, name: "Малиновый XCO", startDate: 1693724478000, raceType: 1},
+]
+
+
