89b051c2f7b6a2546cad1c906841fec6515c94cd
vres271 <vres271@gmail.com>
2023-06-30 23:39:34 +0400

    Pump DATA

--- src/app/shared/mocks/racers.mock.ts
+++ src/app/shared/mocks/racers.mock.ts
@@ -1,35 +1,26 @@
 import { RacerDTO } from '../models/racer.model';
 export const RacersMockData: RacerDTO[] = [
-  {
-    id: 1,
-    userId: 1,
-    raceId: 1,
-    categoryId: 1,
-    regDate: new Date().getTime(),
-    num: 754,
-  },
-  {
-    id: 2,
-    userId: 3,
-    raceId: 3,
-    categoryId: 3,
-    regDate: new Date().getTime(),
-    num: 756,
-  },
-  {
-    id: 4,
-    userId: 4,
-    raceId: 4,
-    categoryId: 4,
-    regDate: new Date().getTime(),
-    num: 758,
-  },
-  {
-    id: 5,
-    userId: 5,
-    raceId: 5,
-    categoryId: 5,
-    regDate: new Date().getTime(),
-    num: 734,
-  }
+
+  {id: 1, userId: 1, num: 1, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 2, userId: 2, num: 2, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 3, userId: 3, num: 3, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 4, userId: 4, num: 4, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 5, userId: 5, num: 5, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 6, userId: 6, num: 6, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 7, userId: 7, num: 7, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 8, userId: 8, num: 8, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 9, userId: 9, num: 9, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 10, userId: 10, num: 10, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 11, userId: 11, num: 11, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 12, userId: 12, num: 12, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 13, userId: 13, num: 13, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 14, userId: 14, num: 14, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 15, userId: 15, num: 15, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 16, userId: 16, num: 16, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 17, userId: 17, num: 17, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 18, userId: 18, num: 18, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 19, userId: 19, num: 19, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 20, userId: 20, num: 20, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+  {id: 21, userId: 21, num: 21, raceId: 2, categoryId: 1, regDate: new Date().getTime()},
+
 ]
