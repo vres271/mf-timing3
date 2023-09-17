c8e273e94b29d47cd72b94dcbf11cbe4e1fbc802
vres271 <vres271@gmail.com>
2023-09-17 23:20:57 +0400

    init: start cssMemeSlider-task LANG=en_EN date='(%a, %b %d, %Y %r)'

--- src/app/shared/models/racer.model.ts
+++ src/app/shared/models/racer.model.ts
@@ -13,7 +13,7 @@ export interface RacerDTO extends Item{
     regDate: number;
     num: number;
 }
-// test
+// test 1
 export class Racer  implements RacerDTO{
     id: number;
     userId: number;
