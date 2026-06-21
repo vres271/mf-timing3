7cb93e38b7bda979d3dbbc2437d1580b259826ae
vres271 <vres271@gmail.com>
2026-06-21 22:04:24 +0400

    Проигрывание звуков, дублирование, перекрытие и т.д.

--- src/app/core/services/audio.service.ts
+++ src/app/core/services/audio.service.ts
@@ -4,22 +4,100 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AudioService {
-  private path = '../../../assets/sounds/';
+  // Абсолютный путь от корня домена
+  private basePath = '/assets/sounds/'; 
   public mute = false;
+
+  // Трекинг активных звуков и таймаутов для возможности их отмены
+  private activeAudios: HTMLAudioElement[] = [];
+  private activeTimeouts: number[] = [];
 
   constructor() { }
 
-  playFile(fileName: string, delay: number = 0, volume: number = 1): void {
-    if (this.mute) {
-        return;
-    }
-    setTimeout(() => {
-        const audio = new Audio();
-        audio.volume = volume;
-        audio.src = this.path+fileName;
-        audio.load();
-        audio.play();
-    }, delay)
+  /**
+   * @param fileName Имя файла
+   * @param delay Задержка в мс
+   * @param volume Громкость 0-1
+   * @param isVoice Если true, прерывает предыдущие голосовые звуки (чтобы не было каши)
+   */
+  playFile(fileName: string, delay: number = 0, volume: number = 1, isVoice: boolean = false): void {
+    if (this.mute) return;
+
+    const timeoutId = window.setTimeout(() => {
+      this.removeTimeout(timeoutId);
+
+      // Если это голосовое объявление, прерываем предыдущие голосовые
+      if (isVoice) {
+        this.stopVoice();
+      }
+
+      const audio = new Audio();
+      audio.volume = volume;
+      audio.src = `${this.basePath}${fileName}`;
+      // Помечаем тип звука для управления
+      audio.dataset['type'] = isVoice ? 'voice' : 'sfx'; 
+
+      this.activeAudios.push(audio);
+
+      // Очищаем память после окончания воспроизведения
+      audio.addEventListener('ended', () => this.removeAudio(audio));
+      audio.addEventListener('error', () => this.removeAudio(audio));
+
+      const playPromise = audio.play();
+      if (playPromise !== undefined) {
+        playPromise.catch(error => {
+          // Браузер может заблокировать автоплей, если не было клика пользователя
+          console.warn(`Не удалось воспроизвести ${fileName}:`, error.message);
+          this.removeAudio(audio);
+        });
+      }
+    }, delay);
+
+    this.activeTimeouts.push(timeoutId);
   }
 
+  // Останавливает всё: и звуки, и очищает очередь таймаутов
+  stopAll() {
+    this.clearPending();
+    this.activeAudios.forEach(audio => {
+      audio.pause();
+      audio.currentTime = 0;
+    });
+    this.activeAudios = [];
+  }
+
+  // Очищает только очередь (таймауты), но дает текущим звукам доиграться
+  clearPending() {
+    this.activeTimeouts.forEach(id => clearTimeout(id));
+    this.activeTimeouts = [];
+  }
+
+  private stopVoice() {
+    const voiceAudios = this.activeAudios.filter(a => a.dataset['type'] === 'voice');
+    voiceAudios.forEach(audio => {
+      audio.pause();
+      audio.currentTime = 0;
+      this.removeAudio(audio);
+    });
+  }
+
+  private removeAudio(audio: HTMLAudioElement) {
+    const index = this.activeAudios.indexOf(audio);
+    if (index > -1) this.activeAudios.splice(index, 1);
+  }
+
+  private removeTimeout(id: number) {
+    const index = this.activeTimeouts.indexOf(id);
+    if (index > -1) this.activeTimeouts.splice(index, 1);
+  }
+
+  // ВАЖНО: Метод для "разблокировки" звука в браузере.
+  // Должен быть вызван по первому клику пользователя в интерфейсе.
+  unlockAudioContext() {
+    const audio = new Audio();
+    audio.muted = true;
+    audio.play().then(() => {
+      audio.pause();
+    }).catch(() => {});
+  }
 }
--- src/app/shared/services/timing.service.ts
+++ src/app/shared/services/timing.service.ts
@@ -9,259 +9,256 @@ import { Observable, Subject, distinctUntilChanged, filter, interval, map, tap }
 import { AudioService } from 'src/app/core/services/audio.service';
 
 export enum TimingType {
-  Laps = 1
+    Laps = 1
 }
 
 export enum TimingState {
-  Unknown,
-  StandBy,
-  Ready,
-  Race,
+    Unknown,
+    StandBy,
+    Ready,
+    Race,
 }
 
 export interface TimingLap {
-  t: number;
-  time: number
+    t: number;
+    time: number
 }
 
 export interface TimerMonitor {
-  t: number;
-  raceTime: number;
-  lapTime: number;
+    t: number;
+    raceTime: number;
+    lapTime: number;
 }
 
 export class TimingTimer {
 
-  startTime: number = 0;
-  finishTime: number = 0;
-  finishT: number = 0;
-  lapsCount: number = 1;
-  laps: TimingLap[];
-  lap: number = 0;
-  started = false;
+    startTime: number = 0;
+    finishTime: number = 0;
+    finishT: number = 0;
+    lapsCount: number = 1;
+    laps: TimingLap[];
+    lap: number = 0;
+    started = false;
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  getMonitor():Observable<TimerMonitor> {
-    return interval(10).pipe(
-      map(() => {
-        const t = new Date().getTime();
-        return {
-          t,
-          raceTime: this.started ? t - this.startTime : this.finishT,
-          lapTime: this.started ? t - (this.previousLap?.time || this.startTime) : this.previousLap?.t,
-        }
-      }),
-      distinctUntilChanged((prev, curr) => prev.raceTime === curr.raceTime),
-    )
-  }
+    getMonitor():Observable<TimerMonitor> {
+        return interval(10).pipe(
+            map(() => {
+                const t = new Date().getTime();
+                return {
+                    t,
+                    raceTime: this.started ? t - this.startTime : this.finishT,
+                    lapTime: this.started ? t - (this.previousLap?.time || this.startTime) : this.previousLap?.t,
+                }
+            }),
+            distinctUntilChanged((prev, curr) => prev.raceTime === curr.raceTime),
+        )
+    }
 
-  get firstLap() {
-    return this.laps?.[0];
-  }
+    get firstLap() {
+        return this.laps?.[0];
+    }
 
-  get curentLap() {
-    return this.laps?.[this.lap];
-  }
+    get curentLap() {
+        return this.laps?.[this.lap];
+    }
 
-  get previousLap() {
+    get previousLap() {
     return this.laps?.[this.lap-1];
-  }
+    }
 
-  get lastLap() {
+    get lastLap() {
     return this.laps?.[this.laps?.length-1];
-  }
-
-  reset(lapsCount?: number) {
-    this.startTime = 0;
-    this.finishTime = 0;
-    this.finishT = 0;
-    this.lap = 0;
-    if (lapsCount) {
-      this.lapsCount = lapsCount;
     }
-    this.laps = [];
-    for (let i = 0; i < this.lapsCount; i++) {
-      this.laps.push({t:0, time: 0});
+
+    reset(lapsCount?: number) {
+        this.startTime = 0;
+        this.finishTime = 0;
+        this.finishT = 0;
+        this.lap = 0;
+        if (lapsCount) {
+            this.lapsCount = lapsCount;
+        }
+        this.laps = [];
+        for (let i = 0; i < this.lapsCount; i++) {
+            this.laps.push({t:0, time: 0});
+        }
     }
-  }
 
-  registerStart() {
-    this.started = true;
-    this.startTime = new Date().getTime();
-  }
+    registerStart() {
+        this.started = true;
+        this.startTime = new Date().getTime();
+    }
 
-  registerLap(t: number) {
-    this.laps[this.lap++] = {t, time: new Date().getTime()}
-  }
+    registerLap(t: number) {
+        this.laps[this.lap++] = {t, time: new Date().getTime()}
+    }
 
-  registerFinish(dt:number) {
-    this.started = false;
-    this.finishTime = new Date().getTime();
-    this.finishT = dt;
-  }
+    registerFinish(dt:number) {
+        this.started = false;
+        this.finishTime = new Date().getTime();
+        this.finishT = dt;
+    }
 
 }
 
 @Injectable()
 export class TimingService {
-  type = TimingType.Laps;
-  state = TimingState.Unknown;
-  racer: Racer | EmptyRacer;
-  race: Race;
+    type = TimingType.Laps;
+    state = TimingState.Unknown;
+    racer: Racer | EmptyRacer;
+    race: Race;
 
-  connected = false;
-  timer: TimingTimer;
+    connected = false;
+    timer: TimingTimer;
 
-  autoStandByAfterFinish = true;
-  lastTimePersonalSoundPlayed = 0;
-  lastIndexPersonalSoundPlayed: number;
+    autoStandByAfterFinish = true;
+    lastTimePersonalSoundPlayed = 0;
+    lastIndexPersonalSoundPlayed: number;
 
-  constructor(
-    private timecontrolAPIService: TimecontrolAPIService,
-    private racersService: RacersService,
-    private raceEventsService: RaceEventsService,
-    private audioService: AudioService,
-  ) {
-    this.timer = new TimingTimer();
-    this.init();
-  }
-
-  connect() {
-    this.timecontrolAPIService.connect();
-    return this.timecontrolAPIService.connected$;
-  }
-
-  setRace(race: Race) {
-    if (this.state !== TimingState.Unknown) {
-      return;
+    constructor(
+        private timecontrolAPIService: TimecontrolAPIService,
+        private racersService: RacersService,
+        private raceEventsService: RaceEventsService,
+        private audioService: AudioService,
+    ) {
+        this.timer = new TimingTimer();
+        this.init();
     }
-    this.race = race;
-  }
 
-  getLaps() {
-    return this.timer.laps;
-  }
-
-  init() {
-
-    this.timecontrolAPIService.connected$
-      .subscribe(connected => {
-        if (connected) {
-          this.connected = true;
-          this.state = TimingState.StandBy;
-        }
-      })
-
-    this.timecontrolAPIService.output()
-      .subscribe(tcMessage => {
-        switch (tcMessage?.command) {
-          case TimecontrolOutputCommand.READY:
-            this.state = TimingState.Ready;
-            if (tcMessage.laps) {
-              this.timer.reset(tcMessage.laps);
-            }
-            break;
-          case TimecontrolOutputCommand.IN_MENU:
-            this.state = TimingState.StandBy;
-            break;
-          case TimecontrolOutputCommand.SET_RACER:
-            if (tcMessage.racer) {
-              this.racer = this.racersService.getCachedByNum(tcMessage.racer) || {num: tcMessage.racer};
-              this.timer.reset();
-            }
-            break;
-          case TimecontrolOutputCommand.START:
-            this.state = TimingState.Race;
-            this.timer.reset();
-            this.timer.registerStart();
-            this.addRaceEvent(tcMessage, RaceEventType.Start);
-            this.audioService.playFile(`beep_start.mp3`, 0, .5);
-            this.audioService.playFile(`start.mp3`,1000);
-            break;
-          case TimecontrolOutputCommand.LAP:
-            this.timer.registerLap(tcMessage.time || 0);
-            this.addRaceEvent(tcMessage, RaceEventType.Point);
-            if (this.timer?.lap < this.timer?.lapsCount) {
-              this.audioService.playFile(`beep_lap.mp3`, 0, .6);
-              this.audioService.playFile(`lap_${this.timer?.lap}.mp3`, 1000);
-            }
-            if (this.timer?.lap === this.timer?.lapsCount - 1) {
-              this.audioService.playFile(`last.mp3`, 2000);
-            }
-            break;
-          case TimecontrolOutputCommand.FINISH:
-            this.timer.registerFinish(tcMessage.time || 0);
-            this.state = TimingState.Ready;
-            this.addRaceEvent(tcMessage, RaceEventType.Finish);
-            this.audioService.playFile(`beep_finish.mp3`);
-            this.audioService.playFile(`finish.mp3`, 1000);
-            if (this.autoStandByAfterFinish) {
-              this.setReady()
-            }
-            break;
-          default:
-            break;
-        }
-        this.playPersonalSound(tcMessage);
-      });        
-  }
-
-  private playPersonalSound(tcMessage: TimecontrolOutputDTO) {
-    const racer = this.racersService.getCachedById(tcMessage.racer || 0);
-    switch (tcMessage?.command) {
-        case TimecontrolOutputCommand.START:
-        case TimecontrolOutputCommand.LAP:
-        this.lastTimePersonalSoundPlayed++;
-        const reasonToPlay = (Math.random() < .3 || this.lastTimePersonalSoundPlayed > 5);
-        if (reasonToPlay && this.timer?.lap < this.timer?.lapsCount) {
-          if (/Шатурн/.test(racer.userFullName)) {
-            const soundFileNames = [
-              `dima_ebash.mp3`,
-              `ebash_kak_boghenka.mp3`,
-              `raschlenitel_goril.mp3`,
-              `glyadite_koghanye_meshki.mp3`,
-            ]
-            let randomIndex = Math.floor(Math.random()*soundFileNames.length);
-            if (randomIndex === this.lastIndexPersonalSoundPlayed) {
-              randomIndex = Math.floor(Math.random()*soundFileNames.length);
-            }
-            this.lastIndexPersonalSoundPlayed = randomIndex;
-            this.audioService.playFile(soundFileNames[randomIndex], 3000);
-            this.lastTimePersonalSoundPlayed = 0;
-          }
-        }
-        break;
-      default:
-        break;
+    connect() {
+        this.timecontrolAPIService.connect();
+        return this.timecontrolAPIService.connected$;
     }
-  }
 
-  setReady() {
-    this.timecontrolAPIService.input({
-      cmd: this.state === TimingState.Ready ? TimecontrolInputCommand.UP : TimecontrolInputCommand.SET_READY,
-    });
-  }
+    setRace(race: Race) {
+        if (this.state !== TimingState.Unknown) {
+            return;
+        }
+        this.race = race;
+    }
 
-  setRacer(racer: Racer) {
-    this.timecontrolAPIService.input({
-      cmd: TimecontrolInputCommand.SET_RACER,
-      value: [+racer.num],
-    });
-  }
+    getLaps() {
+        return this.timer.laps;
+    }
 
-  addRaceEvent(mess: TimecontrolOutputDTO, type: RaceEventType) {
-    this.raceEventsService.add({
-      id: 0,
-      date: new Date().getTime(),
-      dt: mess.time || 0,
-      raceEventType: type,
-      raceId: this.race?.id || 0,
-      racerId: this.racer?.id || 0,
-      detail: {cmd_racer: mess.racer},
-    }).subscribe();
-  }
+    init() {
+
+        this.timecontrolAPIService.connected$
+            .subscribe(connected => {
+                if (connected) {
+                    this.connected = true;
+                    this.state = TimingState.StandBy;
+                }
+            })
+
+        this.timecontrolAPIService.output()
+            .subscribe(tcMessage => {
+                switch (tcMessage?.command) {
+                    case TimecontrolOutputCommand.READY:
+                        this.state = TimingState.Ready;
+                        if (tcMessage.laps) {
+                            this.timer.reset(tcMessage.laps);
+                        }
+                        break;
+                    case TimecontrolOutputCommand.IN_MENU:
+                        this.state = TimingState.StandBy;
+                        break;
+                    case TimecontrolOutputCommand.SET_RACER:
+                        if (tcMessage.racer) {
+                            this.racer = this.racersService.getCachedByNum(tcMessage.racer) || {num: tcMessage.racer};
+                            this.timer.reset();
+                        }
+                        break;
+                    case TimecontrolOutputCommand.START:
+                        this.state = TimingState.Race;
+                        this.timer.reset();
+                        this.timer.registerStart();
+                        this.addRaceEvent(tcMessage, RaceEventType.Start);
+                        this.audioService.playFile(`beep_start.mp3`, 0, .5);
+                        this.audioService.playFile(`start.mp3`,1000);
+                        break;
+                    case TimecontrolOutputCommand.LAP:
+                        this.timer.registerLap(tcMessage.time || 0);
+                        this.addRaceEvent(tcMessage, RaceEventType.Point);
+                        if (this.timer?.lap < this.timer?.lapsCount) {
+                            this.audioService.playFile('beep_lap.mp3', 0, .6, false);
+                            this.audioService.playFile(`lap_${this.timer?.lap}.mp3`, 800, 1, true);
+                        }
+                        if (this.timer?.lap === this.timer?.lapsCount - 1) {
+                            this.audioService.playFile('last.mp3', 1800, 1, true);
+                        }
+                        break;
+                    case TimecontrolOutputCommand.FINISH:
+                        this.audioService.clearPending();
+                        this.timer.registerFinish(tcMessage.time || 0);
+                        this.state = TimingState.Ready;
+                        this.addRaceEvent(tcMessage, RaceEventType.Finish);
+                        this.audioService.playFile('beep_finish.mp3', 0, 1, false);
+                        this.audioService.playFile('finish.mp3', 500, 1, true);
+                        if (this.autoStandByAfterFinish) {
+                            this.setReady()
+                        }
+                        break;
+                }
+                this.playPersonalSound(tcMessage);
+            });
+    }
+
+    private playPersonalSound(tcMessage: TimecontrolOutputDTO) {
+        const racer = this.racersService.getCachedById(tcMessage.racer || 0);
+
+        if (tcMessage?.command === TimecontrolOutputCommand.START ||
+            tcMessage?.command === TimecontrolOutputCommand.LAP) {
+
+            this.lastTimePersonalSoundPlayed++;
+            const reasonToPlay = (Math.random() < .3 || this.lastTimePersonalSoundPlayed > 5);
+            if (reasonToPlay && this.timer?.lap < this.timer?.lapsCount) {
+                if (racer?.userFullName && /Шатурн/.test(racer.userFullName)) {
+                    const soundFileNames = [
+                        'dima_ebash.mp3',
+                        'ebash_kak_boghenka.mp3',
+                        'raschlenitel_goril.mp3',
+                        'glyadite_koghanye_meshki.mp3',
+                    ];
+                    let randomIndex = Math.floor(Math.random() * soundFileNames.length);
+                    if (randomIndex === this.lastIndexPersonalSoundPlayed && soundFileNames.length > 1) {
+                        randomIndex = (randomIndex + 1) % soundFileNames.length;
+                    }
+                    this.lastIndexPersonalSoundPlayed = randomIndex;
+                    this.audioService.playFile(soundFileNames[randomIndex], 2500, 1, true);
+                    this.lastTimePersonalSoundPlayed = 0;
+                }
+            }
+        }
+    }
+
+    setReady() {
+        this.timecontrolAPIService.input({
+            cmd: this.state === TimingState.Ready ? TimecontrolInputCommand.UP : TimecontrolInputCommand.SET_READY,
+        });
+    }
+
+    setRacer(racer: Racer) {
+        this.timecontrolAPIService.input({
+            cmd: TimecontrolInputCommand.SET_RACER,
+            value: [+racer.num],
+        });
+    }
+
+    addRaceEvent(mess: TimecontrolOutputDTO, type: RaceEventType) {
+        this.raceEventsService.add({
+            id: 0,
+            date: new Date().getTime(),
+            dt: mess.time || 0,
+            raceEventType: type,
+            raceId: this.race?.id || 0,
+            racerId: this.racer?.id || 0,
+            detail: {cmd_racer: mess.racer},
+        }).subscribe();
+    }
 
 
 
