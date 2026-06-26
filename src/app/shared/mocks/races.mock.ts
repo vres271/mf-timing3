6bc182df5dd6c81bc5b92d277b2d083424353f40
vres271 <vres271@gmail.com>
2026-06-26 21:43:26 +0400

    LCPF2026 MockData

--- src/app/shared/mocks/races.mock.ts
+++ src/app/shared/mocks/races.mock.ts
@@ -1,8 +1,6 @@
 import { RaceDTO } from '../models/race.model';
 export const RacesMockData: RaceDTO[] = [
-  {id: 1, name: "Медицинка XCO", startDate: 1685250000000, raceType: 1},
-  {name: "LCPF 2024 - Левобережный ПампФест", startDate: 1719532800000, raceType: 2, id: 2},
-  {id: 3, name: "Малиновый XCO", startDate: 1693724478000, raceType: 1},
+  {id: 4, name: "LCPF 2026 - Левобережный ПампФест", startDate: 1782561600000, raceType: 2}
 ]
 
 
